874a77476e76668d629aa3fb600eea97bc27158a
Luan Moraes Alencar <luan.moraees21@gmail.com>
2025-09-30 13:25:26 -0300

    solving flake8 issues

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,9 +1,11 @@
 from app.split_integer import split_integer
 
+
 def test_core_logic_result() -> None:
     assert split_integer(6, 2) == [3, 3]
     assert split_integer(17, 4) == [4, 4, 4, 5]
     assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+
 
 def test_sum_of_parts_equals_value() -> None:
     assert sum(split_integer(10, 3)) == 10
@@ -17,7 +19,7 @@ def test_difference_between_max_and_min_is_at_most_one() -> None:
 
 def test_parts_are_sorted_ascending() -> None:
     result = split_integer(20, 6)
-    assert  result == sorted(result)
+    assert result == sorted(result)
 
 
 def test_length_of_result_equals_number_of_parts() -> None:
@@ -32,8 +34,18 @@ def test_split_into_one_part_is_value_itself() -> None:
 def test_value_less_than_parts_contains_zeros() -> None:
     assert split_integer(3, 5) == [0, 0, 1, 1, 1]
 
+
 def test_element_type_is_int() -> None:
     assert all(isinstance(x, int) for x in split_integer(10, 3))
 
+
 def test_nom_negative_results() -> None:
     assert all(x >= 0 for x in split_integer(3, 5))
+
+
+def test_even_divisibility() -> None:
+    assert split_integer(12, 3) == [4, 4, 4]
+
+
+def test_uneven_divisibility() -> None:
+    assert split_integer(5, 3) == [1, 2, 2]
